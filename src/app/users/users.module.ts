f04a379ed90e2e98a1137ac84f1232c37978c847
JosephAntony37900 <Joseantoniopintoaguilar@gmail.com>
2024-09-18 14:52:04 -0600

    Creacion del formulario para añadir usuario e implementacion de estilos

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersDashboardComponent } from './users-dashboard/users-dashboard.component';
 import { FormsModule } from '@angular/forms';
+import { EditUserFormComponent } from './edit-user-form/edit-user-form.component';
 
 
 
 @NgModule({
   declarations: [
-    UsersDashboardComponent
+    UsersDashboardComponent,
+    EditUserFormComponent
   ],
   imports: [
     CommonModule,
